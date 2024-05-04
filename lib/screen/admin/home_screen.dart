9c1fda6f6f55b7770d8d701d8cddffcce465d820
reflan nuari <reflannuari@gmail.com>
2024-05-04 11:18:48 +0700

    commit

--- lib/screen/admin/home_screen.dart
+++ lib/screen/admin/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:project_ti22a/config/asset.dart';
+import 'package:project_ti22a/screen/admin/list_mahasiswa.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -35,7 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    IconButton('User', Icons.people, 0),
+                    IconButton('Mahasiswa', Icons.people, 0),
                     IconButton('Menu 2', Icons.mail, 0),
                     IconButton('Menu 3', Icons.folder, 0),
                     IconButton('Menu 4', Icons.person_add_alt_1, 0),
@@ -56,6 +58,66 @@ class _HomeScreenState extends State<HomeScreen> {
               ],
             ),
           ),
+          Container(
+            margin: EdgeInsets.only(
+              top: 10,
+            ),
+            child: Text(
+              "Berita Terkini",
+              style: TextStyle(
+                color: Asset.colorPrimaryDark,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            alignment: Alignment.topLeft,
+          ),
+          SizedBox(
+            height: 170,
+            child: ListView(
+              children: [
+                CarouselSlider(
+                  items: [
+                    BeritaTerkini(
+                      'https://lampost.co/wp-content/uploads/2023/11/universitas-teknokrat-indonesia-gelar-workshop-metaverse-102.jpg',
+                      'Universitas Teknokrat Indonesia Gelar Workshop Metaverse 102',
+                      '22 November 2023',
+                    ),
+                    BeritaTerkini(
+                      'https://lampost.co/wp-content/uploads/2023/10/membanggakan-mahasiswa-universitas-teknokrat-indonesia-kuliah-di-luar-negeri-350x250.jpg',
+                      'Membanggakan! Mahasiswa Universitas Teknokrat Indonesia Kuliah di Luar Negeri',
+                      '20 September 2023',
+                    ),
+                    BeritaTerkini(
+                      'https://lampost.co/wp-content/uploads/2024/04/uti-1-350x250.jpg',
+                      'Prodi Matematika UTI Sukses Tingkatkan Akreditasi',
+                      '17 April 2024',
+                    ),
+                    BeritaTerkini(
+                      'https://lampost.co/wp-content/uploads/2024/04/uti-350x250.jpg',
+                      'UTI dan BPPTIK Selenggarakan Pelatihan dan Sertifikasi Kompetensi Teknologi',
+                      '03 April 2024',
+                    ),
+                    BeritaTerkini(
+                      'https://lampost.co/wp-content/uploads/2024/02/uti-latih-dosen-dan-tendik-dengan-ilmu-jurnalistik-750x450.jpg.webp',
+                      'UTI Latih Dosen dan Tendik dengan Ilmu Jurnalistik',
+                      '12 Februari 2024',
+                    ),
+                  ],
+                  options: CarouselOptions(
+                    height: 170,
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    aspectRatio: 16 / 9,
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enableInfiniteScroll: true,
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    viewportFraction: 0.8,
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -69,7 +131,9 @@ class IconButton extends StatelessWidget {
 
   IconButton(this.nameLabel, this.iconLabel, this.index);
 
-  List<Map> _fragment = [];
+  List<Map> _fragment = [
+    {'title': 'Data Mahasiswa', 'view': ListMahasiswa()},
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -89,7 +153,14 @@ class IconButton extends StatelessWidget {
                 borderRadius: BorderRadius.all(
                   Radius.circular(40),
                 ),
-                onTap: () {},
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => _fragment[index]['view'],
+                    ),
+                  );
+                },
                 child: Container(
                   // margin: EdgeInsets.all(5),
                   height: 60,
@@ -124,6 +195,80 @@ class IconButton extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class BeritaTerkini extends StatelessWidget {
+  final String img;
+  final String judul;
+  final String tanggal;
+
+  BeritaTerkini(this.img, this.judul, this.tanggal);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                image: DecorationImage(
+                  image: NetworkImage(img),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                gradient: LinearGradient(
+                  colors: [
+                    Asset.colorPrimaryDark,
+                    Colors.transparent,
+                  ],
+                  end: Alignment.topCenter,
+                  begin: Alignment.bottomCenter,
+                  stops: [0.1, 0.9],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    judul,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    tanggal,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      // alignment: Alignment(1, 1),
     );
   }
 }
---
+++ lib/screen/admin/list_mahasiswa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListMahasiswa extends StatefulWidget {
+  const ListMahasiswa({super.key});
+
+  @override
+  State<ListMahasiswa> createState() => _ListMahasiswaState();
+}
+
+class _ListMahasiswaState extends State<ListMahasiswa> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
